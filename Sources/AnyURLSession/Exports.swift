c8bae1effccb9a31868d7b4b6094b650bfee32c2
Brian Michel <brian.michel@gmail.com>
2024-01-26 17:51:35 -0500

    Create exports file

---
+++ Sources/AnyURLSession/Exports.swift
@@ -0,0 +1,8 @@
+#if os(Linux) || os(Windows)
+// Automatically export the imports that people will most likely require when
+// using the AnyURLSession library in their project.
+@_exported import class FoundationNetworking.HTTPURLResponse
+@_exported import class FoundationNetworking.URLResponse
+@_exported import class FoundationNetworking.URLSessionConfiguration
+@_exported import struct FoundationNetworking.URLRequest
+#endif
